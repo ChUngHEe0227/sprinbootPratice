22cd06d928f39419081df72a7d091fea944a2426
ChUngHEe0227 <rogo0227@naver.com>
2020-04-16 23:07:33 +0900

    검색어를 립력할시 네이버 검색 결과 출력

--- src/main/java/com/tutorial/springboot/controller/Mycontroller.java
+++ src/main/java/com/tutorial/springboot/controller/Mycontroller.java
@@ -1,13 +1,17 @@
 package com.tutorial.springboot.controller;
 
+import com.tutorial.springboot.api.BlogSearchApi;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Mycontroller {
-    @RequestMapping("/")
-    public String index() {
-        return  "hello world";
+    @RequestMapping("/{Search_Target}")
+    public String index(@PathVariable String Search_Target) {
+        BlogSearchApi blogSearchApi =new BlogSearchApi();
+
+        return  blogSearchApi.Search(Search_Target);
     }
 
 }
