d9463934561faf07ac5266a2f44b5d7c54a64838
ChUngHEe0227 <rogo0227@naver.com>
2020-04-18 22:48:30 +0900

    카페 뉴스 블로그 글을 출력 하도록 변경

--- src/main/java/com/tutorial/springboot/controller/Mycontroller.java
+++ src/main/java/com/tutorial/springboot/controller/Mycontroller.java
@@ -1,6 +1,12 @@
 package com.tutorial.springboot.controller;
 
+
+
 import com.tutorial.springboot.api.BlogSearchApi;
+import com.tutorial.springboot.api.CafeSearchApi;
+import com.tutorial.springboot.api.NewsSearchApi;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +15,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class Mycontroller {
     @RequestMapping("/{Search_Target}")
     public String index(@PathVariable String Search_Target) {
-        BlogSearchApi blogSearchApi =new BlogSearchApi();
+        BlogSearchApi blogSearchApi = new BlogSearchApi();
+        NewsSearchApi newsSearchApi = new NewsSearchApi();
+        CafeSearchApi cafeSearchApi = new CafeSearchApi();
 
-        return  blogSearchApi.Search(Search_Target);
+        JSONObject blogSearchResult = null;
+        JSONObject newsSearchResult = null;
+        JSONObject cafeSearchResult = null;
+        try {
+            blogSearchResult = new JSONObject(blogSearchApi.Search(Search_Target));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            newsSearchResult = new JSONObject(newsSearchApi.Search(Search_Target));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            cafeSearchResult = new JSONObject(cafeSearchApi.Search(Search_Target));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            return  "blog" + blogSearchResult.get("total").toString()+"\n news"+newsSearchResult.get("total")+"cafe"+cafeSearchResult.get("total") ;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return "sd";
     }
 
 }
