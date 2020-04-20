c44dfac0322776c43e3fa41838fa7373d61c0927
ChUngHEe0227 <rogo0227@naver.com>
2020-04-20 18:57:45 +0900

    Json 파일을 객체로 변환하는 코드 추가

--- src/main/java/com/tutorial/springboot/controller/Mycontroller.java
+++ src/main/java/com/tutorial/springboot/controller/Mycontroller.java
@@ -1,12 +1,16 @@
 package com.tutorial.springboot.controller;
 
 
+import com.google.gson.Gson;
 
+import com.google.gson.GsonBuilder;
+
+import com.tutorial.springboot.Domain.BlogInformation;
+import com.tutorial.springboot.Domain.CafeInformation;
+import com.tutorial.springboot.Domain.Newsinformation;
 import com.tutorial.springboot.api.BlogSearchApi;
 import com.tutorial.springboot.api.CafeSearchApi;
 import com.tutorial.springboot.api.NewsSearchApi;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,35 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class Mycontroller {
     @RequestMapping("/{Search_Target}")
     public String index(@PathVariable String Search_Target) {
+        String string ="";
         BlogSearchApi blogSearchApi = new BlogSearchApi();
         NewsSearchApi newsSearchApi = new NewsSearchApi();
         CafeSearchApi cafeSearchApi = new CafeSearchApi();
 
-        JSONObject blogSearchResult = null;
-        JSONObject newsSearchResult = null;
-        JSONObject cafeSearchResult = null;
-        try {
-            blogSearchResult = new JSONObject(blogSearchApi.Search(Search_Target));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        try {
-            newsSearchResult = new JSONObject(newsSearchApi.Search(Search_Target));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        try {
-            cafeSearchResult = new JSONObject(cafeSearchApi.Search(Search_Target));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        String blogSearchResult = blogSearchApi.Search(Search_Target);
+        String newsSearchResult = newsSearchApi.Search(Search_Target);
+        String cafeSearchResult = cafeSearchApi.Search(Search_Target);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-        try {
-            return  "blog" + blogSearchResult.get("total").toString()+"\n news"+newsSearchResult.get("total")+"cafe"+cafeSearchResult.get("total") ;
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return "sd";
+        Newsinformation newsinformation = gson.fromJson(newsSearchResult, Newsinformation.class);
+        CafeInformation cafeInformation = gson.fromJson(cafeSearchResult, CafeInformation.class);
+        BlogInformation blogInformation = gson.fromJson(blogSearchResult, BlogInformation.class);
+
+        return string;
     }
 
 }
