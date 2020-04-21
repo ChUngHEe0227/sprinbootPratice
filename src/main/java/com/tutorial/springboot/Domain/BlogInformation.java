9a7564dd3163bda864bc69989931da29c2981b83
ChUngHEe0227 <rogo0227@naver.com>
2020-04-21 23:12:23 +0900

    자료형 변경

--- src/main/java/com/tutorial/springboot/Domain/BlogInformation.java
+++ src/main/java/com/tutorial/springboot/Domain/BlogInformation.java
@@ -5,18 +5,20 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.List;
 
+@Data
 public class BlogInformation {
-    private List<Newsinformation.item> items;
+    private List<items> item;
     private DateTimeFormat lastBuild;
     private Integer total;
     private Integer start;
     private Integer display;
 
     @Data
-    public class item{
+    public class items{
         private String title;
         private String link;
         private String description;
+        private DateTimeFormat postdate;
     }
 
 }
--- src/main/java/com/tutorial/springboot/Domain/CafeInformation.java
+++ src/main/java/com/tutorial/springboot/Domain/CafeInformation.java
@@ -6,16 +6,17 @@ import org.springframework.format.annotation.DateTimeFormat;
 import java.util.List;
 
 public class CafeInformation {
-    private List<Newsinformation.item> items;
+    private List<items> item;
     private DateTimeFormat lastBuild;
     private Integer total;
     private Integer start;
     private Integer display;
 
     @Data
-    public class item{
+    public class items{
         private String title;
         private String link;
         private String description;
+        private String cafename;
     }
 }
